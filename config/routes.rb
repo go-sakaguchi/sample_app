3285ad2816523ff9685dd89a36e12476323072cd
go-sakaguchi <123789515+go-sakaguchi@users.noreply.github.com>
2023-05-16 04:01:46 +0000

    [Finish]3-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  post 'lists' => 'lists#create'   #ここを追記します
-  get 'lists' => 'lists#index'
-  get 'lists/edit'
+
   get '/top' => 'homes#top'
-  # .../lists/1 や .../lists/3 に該当する
-  get 'lists/:id' => 'lists#show', as: 'list'
+  resources :lists
 end
